a322302cf443e89eb6bb96d0c57995ea41a37456
Steve Faulkner <southpolesteve@gmail.com>
2012-06-12 15:06:56 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madisonapis::Application.config.secret_token = '07b587ddfce304d601f57fb361979a9664c4085e363d25f1fa098456b004b07c8cead3d7447dd7f3498aa737113a6244214d4f89538fa6b92477a8c8cf881a09'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Madisonapis::Application.routes.draw do
+  root :to => "home#index"
+end
