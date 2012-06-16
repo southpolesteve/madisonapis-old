bf241f6d50ac6c117858eaa3acdd2bee7c52363d
Steve Faulkner <southpolesteve@gmail.com>
2012-06-16 16:24:01 -0500

    Show beaches as unmonitored

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -6,11 +6,13 @@ class Beach
     beaches = []
     agent = Mechanize.new
     @results = agent.get("http://www.publichealthmdc.com/environmental/water/beaches/")
-    @results.search('.outline_box_beaches').each do |beach|
-      new_beach = Beach.new
-      new_beach.name = beach.search('h2').text
-      new_beach.status = beach.search('.status_open').empty? ? "closed" : "open"
-      beaches << new_beach
+    @results.search('.outline_box_beaches').each do |el|
+      beach = Beach.new
+      beach.name = el.search('h2').text
+      beach.status = "open" if el.search('.status_open').any?
+      beach.status = "closed" if el.search('.status_closed').any?
+      beach.status = "unmonitored" unless beach.status
+      beaches << beach
     end
     beaches
   end
